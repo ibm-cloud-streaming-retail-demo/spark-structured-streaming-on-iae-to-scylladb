bb245f94c343d304f470081184b79c6ff06db6cf
Chris Snow <chsnow123@gmail.com>
2018-11-01 11:04:19 +0000

    update for spark 2.3

--- src/main/scala/cassandra/CassandraDriver.scala
+++ src/main/scala/cassandra/CassandraDriver.scala
@@ -1,12 +1,18 @@
 package cassandra
 
-import org.apache.spark.sql._
+import org.apache.spark.sql.cassandra._
 import com.datastax.spark.connector._
 import com.datastax.spark.connector.cql.CassandraConnector
-import kafka.KafkaService
 import invoices.{InvoiceItem, InvoiceItemKafka}
-import spark.SparkHelper
+import kafka.KafkaService
 import log.LazyLogger
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql._
+import org.apache.spark.sql.catalyst.CatalystTypeConverters
+import org.apache.spark.sql.execution.streaming.Sink
+import org.apache.spark.sql.sources.StreamSinkProvider
+import org.apache.spark.sql.streaming.OutputMode
+import spark.SparkHelper
 
 object CassandraDriver extends LazyLogger {
   private val spark = SparkHelper.getSparkSession()
@@ -30,9 +36,17 @@ object CassandraDriver extends LazyLogger {
     ds
       .toDF() //@TODO see if we can use directly the Dataset object
       .writeStream
-      .format("cassandra.StreamSinkProvider.CassandraSinkProvider")
-      .outputMode("update")
+      .format("cassandra.ScyllaSinkProvider")
+      .outputMode(OutputMode.Append)
       .queryName("KafkaToCassandraStreamSinkProvider")
+      .options(
+        Map(
+          "keyspace" -> namespace,
+          "table" -> StreamProviderTableSink,
+          "checkpointLocation" -> "/tmp/checkpoints"
+        )
+      )
+
       .start()
   }
 
@@ -79,5 +93,46 @@ object CassandraDriver extends LazyLogger {
           }
          }
       """.replaceAll("\n", "").replaceAll(" ", "")
+  }
+}
+
+class ScyllaSinkProvider extends StreamSinkProvider {
+  override def createSink(sqlContext: SQLContext,
+                          parameters: Map[String, String],
+                          partitionColumns: Seq[String],
+                          outputMode: OutputMode): ScyllaSink =
+    new ScyllaSink(parameters)
+}
+
+class ScyllaSink(parameters: Map[String, String]) extends Sink {
+  override def addBatch(batchId: Long, data: DataFrame): Unit = {
+
+    val schema = data.schema
+
+    val rdd: RDD[Row] = data.queryExecution.toRdd.mapPartitions { rows =>
+      val converter = CatalystTypeConverters.createToScalaConverter(schema)
+      rows.map(converter(_).asInstanceOf[Row])
+    }
+
+    val spark = SparkHelper.getSparkSession()
+    val newDF = spark.sqlContext.createDataFrame(rdd, schema)
+
+    newDF.select(
+      newDF.col("invoiceitem.InvoiceNo"),
+      newDF.col("invoiceitem.StockCode"),
+      newDF.col("invoiceitem.Description"),
+      newDF.col("invoiceitem.Quantity"),
+      newDF.col("invoiceitem.InvoiceDate"),
+      newDF.col("invoiceitem.UnitPrice"),
+      newDF.col("invoiceitem.CustomerID"),
+      newDF.col("invoiceitem.Country"),
+      newDF.col("invoiceitem.LineNo"),
+      newDF.col("invoiceitem.InvoiceTime"),
+      newDF.col("invoiceitem.StoreID"),
+      newDF.col("invoiceitem.TransactionID")
+    ).write.cassandraFormat(
+      parameters("table"),
+      parameters("keyspace")
+    ).mode(SaveMode.Append).save()
   }
 }
